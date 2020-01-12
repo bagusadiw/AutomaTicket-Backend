56918daebbd997561b6920bf2272addbc8902f4e
bagusadiw <bagusadi1494@gmail.com>
2020-01-12 22:00:38 +0700

    deploy setup

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ require("express-group-routes")
 
 //use express in app variable
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 // jwt middleware
 const { authenticated } =require('./middleware')
